9dee731f6d0c12b7bf0e3e053a46343763e463f9
Wakaru_Science <65952518+Wakaru-Science@users.noreply.github.com>
2020-05-30 17:24:35 +0530

    Add image

--- assets/Vectors/Multiplication.py
+++ assets/Vectors/Multiplication.py
@@ -29,24 +29,39 @@ y_direct = [0, 0, 0,
 #x1 , y1 = [1,2, 3.4], [1,2,4.3]
 #x2 , y2 = [3,4,0.7], [3,4,2.8]
 
-plt.plot([0.4, 0], [0, 1], color="k", alpha=0.5)
-plt.plot([1.35, 1.85], [0, 1], color="k", alpha=0.5)
+plt.plot([0.4, 0], [0, 1], color="k", alpha=0.4)
+plt.plot([1.35, 1.85], [0, 1], color="k", alpha=0.4)
 
-plt.plot([2.4, 2], [0, 1], color="k", alpha=0.5)
-plt.plot([3.35, 3.85], [0, 1], color="k", alpha=0.5)
+plt.plot([2.4, 2], [0, 1], color="k", alpha=0.4)
+plt.plot([3.35, 3.85], [0, 1], color="k", alpha=0.4)
 
-plt.plot([4.4, 4], [0, 1], color="k", alpha=0.5)
-plt.plot([5.35, 5.85], [0, 1], color="k", alpha=0.5)
+plt.plot([4.4, 4], [0, 1], color="k", alpha=0.4)
+plt.plot([5.35, 5.85], [0, 1], color="k", alpha=0.4)
 
-plt.plot([1, 0], [3, 2], color="k", alpha=0.5)
-plt.plot([1.9, 2.85], [3, 2], color="k", alpha=0.5)
+plt.plot([1, 0], [3, 2], color="k", alpha=0.4)
+plt.plot([1.9, 2.85], [3, 2], color="k", alpha=0.4)
 
-plt.plot([4, 3], [3, 2], color="k", alpha=0.5)
-plt.plot([4.9, 5.80], [3, 2], color="k", alpha=0.5)
+plt.plot([4, 3], [3, 2], color="k", alpha=0.4)
+plt.plot([4.9, 5.80], [3, 2], color="k", alpha=0.4)
 
 
-ax.arrow(7, -0.5, 0, 1.9, head_width=0.2, head_length=0.1, color="k", alpha=0.5)
-ax.arrow(7, 3.5, 0, -1.9, head_width=0.2, head_length=0.1, color="k", alpha=0.5)
+ax.arrow(7, -0.5, 0, 1.9, head_width=0.2, head_length=0.1, color="k", alpha=0.6)
+ax.arrow(7, 3.5, 0, -1.9, head_width=0.2, head_length=0.1, color="k", alpha=0.6)
+
+ax.arrow(2, -0.5, -0.4, 0.3, head_width=0.2, head_length=0.1, color="k", alpha=0.4)
+ax.arrow(2.9, -0.5, 0.0, 0.3, head_width=0.2, head_length=0.1, color="k", alpha=0.4)
+ax.arrow(3.8, -0.5, +0.4, 0.3, head_width=0.2, head_length=0.1, color="k", alpha=0.4)
+
+ax.arrow(1, 1, 0, 0.3, head_width=0.2, head_length=0.1, color="k", alpha=0.4)
+ax.arrow(3, 1, 0, 0.3, head_width=0.2, head_length=0.1, color="k", alpha=0.4)
+ax.arrow(5, 1, 0, 0.3, head_width=0.2, head_length=0.1, color="k", alpha=0.4)
+
+ax.arrow(1.5, 2, 0, -0.3, head_width=0.2, head_length=0.1, color="k", alpha=0.4)
+ax.arrow(4.5, 2, 0, -0.3, head_width=0.2, head_length=0.1, color="k", alpha=0.4)
+
+ax.arrow(2.7, 3.5, -0.5, -0.3, head_width=0.2, head_length=0.1, color="k", alpha=0.4)
+ax.arrow(3.2, 3.5, 0.5, -0.3, head_width=0.2, head_length=0.1, color="k", alpha=0.4)
+
 
 ax.text(2.9,-0.75,"3i")
 ax.text(2.9,3.75,"2i")
@@ -69,9 +84,12 @@ ax.quiver(x_pos,y_pos,x_direct,y_direct,
 
 ax.axis([-1.5, 10, -1, 4])
 
-##plt.grid(which="both")
 ax.set_xticks([])
 ax.set_yticks([])
 
+#plt.grid(which="both")
+#ax.set_xticks(range(-1,11))
+#ax.set_yticks(range(5))
+            
 plt.show()
 
