134c8b1a0881c42fde03547daa70fc466941285f
Wakaru_Science <65952518+Wakaru-Science@users.noreply.github.com>
2020-05-31 13:12:41 +0530

    Mathjax

--- assets/Vectors/Addition_3D.py
+++ assets/Vectors/Addition_3D.py
@@ -2,14 +2,30 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-soa = np.array([[0, 0, 1, 1, -2, 0], [0, 0, 2, 1, 1, 0],
-                [0, 0, 3, 2, 1, 0], [0, 0, 4, 0.5, 0.7, 0]])
+soa = np.array([[0, 0, 0, 1, 2, 0], [0, 0, 0, 1, 3, 0]])
+soa2 = np.array([[0, 0, 0, 2, 5, 0], [0, 0, 0, 1, 6, 1], [0, 0, 0, 1, 6, -1]])
 
 X, Y, Z, U, V, W = zip(*soa)
+X2, Y2, Z2, U2, V2, W2 = zip(*soa2)
+
+print(X, Y, Z, U, V, W)
+
 fig = plt.figure()
+
 ax = fig.add_subplot(111, projection='3d')
-ax.quiver(X, Y, Z, U, V, W)
-ax.set_xlim([-1, 0.5])
-ax.set_ylim([-1, 1.5])
-ax.set_zlim([-1, 8])
+
+ax.quiver(X, Y, Z, U, V, W, length=2, #scale_units="xy", angles="xy",
+          arrow_length_ratio=0.1)
+
+ax.quiver(X2, Y2, Z2, U2, V2, W2, length=1, #scale_units="xy", angles="xy",
+          arrow_length_ratio=0.1, alpha=0.5, color="k")
+
+ax.set_xlim([-7, 7])
+ax.set_ylim([-7, 7])
+ax.set_zlim([-7, 7])
+ 
+#ax.yaxis.set_ticks_position("left")
+
 plt.show()
+
+
